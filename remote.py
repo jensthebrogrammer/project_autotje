06a73734bd60a0c48a2efdb6bc1ed77953f78c09
jensthebrogrammer <jenslaurijssen@gmail.com>
2025-03-19 12:42:21 +0100

    remote afgemaatk en mijn huidige hex file erbij gezet

--- remote.py
+++ remote.py
@@ -1,18 +1,16 @@
 from microbit import *
 from radio_com import RadioCom as Com
-from push_button import PushButton as Btn
 
 
 class Remote(Com):
-    def __init__(self, pinT, pinB, pinL, pinR, pull_up=False, channel=1):
+    def __init__(self, channel=1):
         # declaring super for inheritance
         super().__init__(channel)
 
         # declaring every button
-        self._btnT = Btn(pinT, pull_up=pull_up)
-        self._btnB = Btn(pinB, pull_up=pull_up)
-        self._btnL = Btn(pinL, pull_up=pull_up)
-        self._btnR = Btn(pinR, pull_up=pull_up)
+        self._btnT = pin_logo
+        self._btnA = pin5
+        self._btnB = pin11
 
         # turn the radio on
         self.on()
@@ -20,7 +18,9 @@ class Remote(Com):
     # reading the buttons and sending the correct message
     def update(self):
         # the function write sends it via the radio channel
-        self.write(str(int(self._btnT.is_pressed())))
-        self.write(str(int(self._btnR.is_pressed())))
-        self.write(str(int(self._btnB.is_pressed())))
-        self.write(str(int(self._btnL.is_pressed())))
+
+        # the message sends a string of bits (010 for example). 0 means not pressed. the order is top, left, right
+        message = str(int(self._btnT.is_touched())) + str(int(not self._btnA.read_digital())) + str(
+            int(not self._btnB.read_digital()))
+
+        self.write(message)
